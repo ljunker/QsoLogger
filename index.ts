df12a578e4788cbc80da7173fe8ca24a8354ff48
Lars Junker <larsjunker92@gmail.com>
2023-11-23 15:06:06 +0100

    adding mongodb instead of in-memory db

--- index.ts
+++ index.ts
@@ -1,21 +1,43 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json());
-app.use(express.static('public')); // Serve static files
+app.use(express.static('public'));
 
-let qsoLogs = []; // In-memory array to store QSO logs
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/qsoLogger', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a Mongoose schema for QSO logs
+const qsoSchema = new mongoose.Schema({
+    callSign: String,
+    frequency: String,
+    mode: String,
+    date: { type: Date, default: Date.now }
+});
+
+// Create a model from the schema
+const QSO = mongoose.model('QSO', qsoSchema);
 
 // Endpoint to add a new QSO log
-app.post('/qso', (req, res) => {
-    const qsoLog = req.body;
-    qsoLogs.push(qsoLog);
-    res.status(201).send('QSO logged successfully');
+app.post('/qso', async (req, res) => {
+    try {
+        const newQSO = new QSO(req.body);
+        await newQSO.save();
+        res.status(201).send('QSO logged successfully');
+    } catch (error) {
+        res.status(500).send('Error saving QSO');
+    }
 });
 
 // Endpoint to get all QSO logs
-app.get('/qso', (req, res) => {
-    res.status(200).json(qsoLogs);
+app.get('/qso', async (req, res) => {
+    try {
+        const qsoLogs = await QSO.find({});
+        res.status(200).json(qsoLogs);
+    } catch (error) {
+        res.status(500).send('Error retrieving QSOs');
+    }
 });
 
 const PORT = 3000;
