803798ba57d34ced82e2d9637f473c8579fd1b72
Lars Junker <larsjunker92@gmail.com>
2023-11-23 14:06:35 +0100

    add web page to log and show QSOs

--- index.ts
+++ index.ts
@@ -2,8 +2,23 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
+app.use(express.static('public')); // Serve static files
 
-const PORT = process.env.PORT || 3000;
+let qsoLogs = []; // In-memory array to store QSO logs
+
+// Endpoint to add a new QSO log
+app.post('/qso', (req, res) => {
+    const qsoLog = req.body;
+    qsoLogs.push(qsoLog);
+    res.status(201).send('QSO logged successfully');
+});
+
+// Endpoint to get all QSO logs
+app.get('/qso', (req, res) => {
+    res.status(200).json(qsoLogs);
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
